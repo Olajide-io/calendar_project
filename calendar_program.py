227064694fdd670e1e9924abc8b8eb25bfbb803c
Olajide.io <contactolajide>
2022-03-26 12:40:46 -0500

    Simplying the code

--- calendar_program.py
+++ calendar_program.py
@@ -1,29 +1,8 @@
+# Importing the calendar module
 import calendar
-calendar.setfirstweekday(calendar.SUNDAY)
-year = 2005
-month = 7
-calen = calendar.monthcalendar(year, month)
 
-# Creating an header for the calendar
-print('|------ %s-%s ----|' % ("Aug", year))
-print('|Su Mo Tu We Th Fr Sa|')
-print('|--------------------|')
-
-border = '|'
-for week in calen:
-    line = border
-
-      
-    for day in week:
-        if day == 0: 
-      # Creating 3 spaces for the empty days       
-            line += '   ' 
-        elif len(str(day)) == 1:
-            line += ' %d ' % day
-        else:
-         line += '%d ' % day
-    # Making sure spaces in the last column are removed
-    line = line[0:len(line) - 1]  
-    line += border
-    print(line)
-
+# Creating the object my_calendar from the calendar module 
+my_calendar = calendar.TextCalendar(calendar.SUNDAY)
+# Making sure the start of the month is Sunday
+august_2005_calandar = my_calendar.formatmonth(2005, 8)
+print(august_2005_calandar)
